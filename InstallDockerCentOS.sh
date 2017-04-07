a36e794bd0e38fa9a3183243f9799860edf4b269
Benny Lo <bennymelb@gmail.com>
2017-04-07 16:24:41 +1000

    Update InstallDockerCentOS.sh

--- InstallDockerCentOS.sh
+++ InstallDockerCentOS.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+
+# This script is to install Docker-CE for CentOS
+# https://docs.docker.com/engine/installation/linux/centos/
+
+# Removing old version of docker if exist
 yum remove -y docker docker-common container-selinux docker-selinux docker-engine
+
+# Install yum-utils, which provides the yum-config-manager utility:
 yum install -y yum-utils
+
+# Add the docker ce repo
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# Update yum package index
 yum makecache fast
-yum install docker-ce
+
+# Install Docker CE
+yum -y install docker-ce
