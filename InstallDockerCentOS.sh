a7ee65d2fd14bf3c18e907484f5b708091ef1339
Benny Lo <bennymelb@gmail.com>
2017-04-07 16:11:51 +1000

    Update InstallDockerCentOS.sh

--- InstallDockerCentOS.sh
+++ InstallDockerCentOS.sh
@@ -1,3 +1,4 @@
+yum remove -y docker docker-common container-selinux docker-selinux docker-engine
 yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum makecache fast
