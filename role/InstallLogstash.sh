17e0e1bd3934fdcbf64154926f48e61d1b125652
Benny Lo <bennymelb@gmail.com>
2017-07-04 00:20:30 +1000

    make elastic repo disabled by default

--- role/InstallLogstash.sh
+++ role/InstallLogstash.sh
@@ -16,7 +16,7 @@ echo "name=Elasticsearch repository for 5.x packages" >> $repofile
 echo "baseurl=https://artifacts.elastic.co/packages/5.x/yum" >> $repofile
 echo "gpgcheck=1" >> $repofile
 echo "gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch" >> $repofile
-echo "enabled=1" >> $repofile
+echo "enabled=0" >> $repofile
 echo "autorefresh=1" >> $repofile
 echo "type=rpm-md" >> $repofile
 
