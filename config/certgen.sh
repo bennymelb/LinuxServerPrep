0f881570b87b2bb259f103383bd1642529f7c73b
Benny Lo <bennymelb@gmail.com>
2017-07-06 15:32:36 +1000

    Update certgen.sh

--- config/certgen.sh
+++ config/certgen.sh
@@ -6,4 +6,4 @@ fqdn=$(hostname -A)
 hostname=$(hostname)
 
 # Generate a self sign cert
-sudo openssl req -subj "/CN=$fqdn/" -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout /etc/pki/tls/private/$hostname.key -out /etc/pki/tls/certs/$hostname.crt
+openssl req -subj "/CN=$fqdn/" -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout /etc/pki/tls/private/$hostname.key -out /etc/pki/tls/certs/$hostname.crt
